f62e9b00168c4135323cbf86868df58db833455d
Khanhkudo02 <nguyenkhanh150802@gmail.com>
2024-07-17 17:20:25 +0700

    Dev_NguyenHuuKhanh_17072024

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Admin() {
+  return (
+    <div>
+      <Menu
+        item={[
+          { lable: "Home" },
+          { lable: "Home" },
+          { lable: "Home" },
+          { lable: "Home" },
+          { lable: "Home" },
+        ]}
+      ></Menu>
+      <div>hihihi</div>
+    </div>
+  );
+}
+
+export default Admin;
--- src/service/authService.js
+++ src/service/authService.js
@@ -1,9 +1,52 @@
+// // src/authService.js
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
+// import { ref, set, get } from "firebase/database";
+// import { auth, database } from "../../firebaseConfig";
+
+// export const signUp = async (email, password, role) => {
+//   try {
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     await set(ref(database, "users/" + user.uid), {
+//       email: email,
+//       role: role,
+//     });
+//     return user;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const logIn = async (email, password) => {
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     const snapshot = await get(ref(database, "users/" + user.uid));
+//     const userData = snapshot.val();
+//     console.log("user", user);
+//     console.log("userData", userData);
+//     return { user, userData };
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 // src/authService.js
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { ref, set, get } from "firebase/database";
+import { ref, set, get, update } from "firebase/database";
 import { auth, database } from "../../firebaseConfig";
 
 export const signUp = async (email, password, role) => {
@@ -37,6 +80,15 @@ export const logIn = async (email, password) => {
     console.log("user", user);
     console.log("userData", userData);
     return { user, userData };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateUserRole = async (uid, role) => {
+  try {
+    await update(ref(database, "users/" + uid), { role: role });
+    console.log(`User ${uid} role updated to ${role}`);
   } catch (error) {
     throw error;
   }
