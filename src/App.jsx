c4a65969e813fb8b01cd7bd08367736d7a2942d7
Khanhkudo02 <nguyenkhanh150802@gmail.com>
2024-07-22 09:16:52 +0700

    Upload link

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,79 @@
-// App.jsx
-import React, { useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Layout, theme } from "antd";
+import Sidebar from "./Components/Sidebar";
+import LoginPage from "./pages/LoginPage";
 import Admin from "./pages/Admin";
+import ForgetPassword from "./pages/ForgetPassword";
+import AccountManagement from "./Components/AccountManagement";
+import EmployeeManagement from "./Components/EmployeeManagement";
+import ProjectManagement from "./Components/ProjectManagement";
+import PositionManagement from "./Components/PositionManagement";
+import TechnologyManagement from "./Components/TechnologyManagement";
+import ProgramingLanguage from "./Components/ProgramingLanguage";
+import PageCV from "./pages/PageCV";
 import Employee from "./pages/Employee";
-import Login from "./pages/LoginPage";
 
-function App() {
-  const [user, setUser] = useState(null);
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login setUser={setUser} />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/employee" element={<Employee />} />
-        <Route path="/" element={<Login setUser={setUser} />} />
-      </Routes>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sidebar />
+        <Layout style={{ marginLeft: 200 }}>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            <div
+              style={{
+                padding: 24,
+                background: colorBgContainer,
+              }}
+            >
+              <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/cv" element={<PageCV />} />
+                <Route path="/employee" element={<Employee />} />
+                <Route path="/forget-password" element={<ForgetPassword />} />
+                <Route
+                  path="/account-management"
+                  element={<AccountManagement />}
+                />
+                <Route
+                  path="/employee-management"
+                  element={<EmployeeManagement />}
+                />
+                <Route
+                  path="/project-management"
+                  element={<ProjectManagement />}
+                />
+                <Route
+                  path="/position-management"
+                  element={<PositionManagement />}
+                />
+                <Route
+                  path="/technology-management"
+                  element={<TechnologyManagement />}
+                />
+                <Route
+                  path="/programing-language"
+                  element={<ProgramingLanguage />}
+                />
+              </Routes>
+            </div>
+          </Content>
+          {/* <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer> */}
+        </Layout>
+      </Layout>
     </Router>
   );
-}
+};
 
 export default App;
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LogoutButton from "../Components/LogoutButton";
-
+import Sidebar from "../Components/Sidebar";
 
 function Employee() {
   return (
     <div>
       <h1>This is employee page</h1>
+      <Sidebar />
       <LogoutButton></LogoutButton>
     </div>
   );
