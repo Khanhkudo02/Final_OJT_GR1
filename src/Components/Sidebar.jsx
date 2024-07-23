6bec8b75ddefa3ab6ca972c2d9e2eaf3dfe093f1
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2024-07-23 08:57:32 +0700

    Change file

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -106,11 +106,12 @@ const Sidebar = () => {
     >
       <div className="sidebar-header">
         <img
-          src="public\images\Logo-GetIT.jpg"
+          src="/public/images/logo.jpg"
           alt="Get IT"
           className="logo-sidebar"
-          style={{ width: "50px", height: "60px" }}
+          style={{ width: "50px", height: "40px" }}
         />
+        <h2 className="sidebar-title">GETIT COMPANY</h2>
       </div>
       <Menu
         theme="dark"
