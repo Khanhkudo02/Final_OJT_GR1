b2948e6861ff1a8c209f52a72183607a3ecd02f2
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2024-07-25 13:48:40 +0700

    Change file

---
+++ src/templates/templateCV2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/style/template/templateCV1=2.scss'
+
+function templateCV2() {
+  return (
+    <div>templateCV2</div>
+  )
+}
+
+export default templateCV2
