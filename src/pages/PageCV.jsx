0f0d2fa69f17834e47571f34bf740deb59ff9bcf
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2024-07-25 17:03:51 +0700

    Add: Add new template CV2

--- src/pages/PageCV.jsx
+++ src/pages/PageCV.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import "../assets/style/Pages/PageCV.scss";
 import avt from "../../public/images/avatar.jpg";
+import TemplateCV1 from "../templates/templateCV1";
+import TemplateCV2 from "../templates/templateCV2";
 
 const loadFont = async (url) => {
   const response = await fetch(url);
@@ -162,12 +164,50 @@ function PageCV() {
     doc.save("cv.pdf");
   };
 
+  const [selectedTemplate, setSelectedTemplate] = useState('');
+  const [templateContent, setTemplateContent] = useState(null);
+
+  const handleTemplateChange = (e) => {
+    const template = e.target.value;
+    setSelectedTemplate(template);
+
+    // Load the selected template content
+    if (template === 'Template1') {
+      setTemplateContent(<TemplateCV1 />);
+    } else if (template === 'Template2') {
+      setTemplateContent(<TemplateCV2 />);
+    } else {
+      setTemplateContent(null);
+    }
+  };
+
+  const exportPDF = () => {
+    const doc = new jsPDF();
+    if (selectedTemplate === 'Template1') {
+      TemplateCV1.generatePDF(doc);
+    } else if (selectedTemplate === 'Template2') {
+      TemplateCV2.generatePDF(doc);
+    }
+    doc.save('CV.pdf');
+  }
+
   return (
     <div>
       <div className="PDF-Button">
-        <button className="download-btn" onClick={handleDownloadPdf}>
+        {/* <button className="download-btn" onClick={handleDownloadPdf}>
+          Download PDF
+        </button> */}
+        <select className="select-CV" onChange={handleTemplateChange} value={selectedTemplate}>
+          <option className="option-CV" value="">Select a template</option>
+          <option className="option-CV" value="Template1">Template 1</option>
+          <option className="option-CV" value="Template2">Template 2</option>
+        </select>
+        <button className="download-btn" onClick={exportPDF}>
           Download PDF
         </button>
+        <div>
+          {templateContent}
+        </div>
       </div>
     </div>
   );
