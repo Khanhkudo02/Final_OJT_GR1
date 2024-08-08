c95a95128f5ceadef0702c70911c1ccd81593511
ssphat <sayphat1402@gmail.com>
2024-08-08 08:43:25 +0700

    Add translate admin

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const EMAILJS_USER_ID = 'XbI9znGx2_ZV3wfBO';
-export const EMAILJS_SERVICE_ID = 'service_kdlrww8';
-export const EMAILJS_TEMPLATE_ID = 'template_yh7totx';
+export const EMAILJS_USER_ID = 'RSDnD2F8I4qw38cFd';
+export const EMAILJS_SERVICE_ID = 'service_9r2qdij';
+export const EMAILJS_TEMPLATE_ID = 'template_orarn6c';
